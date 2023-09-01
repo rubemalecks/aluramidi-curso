73247da2ea11f3a6ed07f5d139e4c48a105fca30
Rubem Álecks <rubemalecks@gmail.com>
2023-09-01 14:52:17 -0300

    ...

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert('olá mundo!');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+
+    const instrumento =  listaDeTeclas[contador].classList[1];
+    console.log()
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(contador);
+    }
+    contador += 1
+
+    console.log(contador)
+}
+
